156670d70faac4e1c018270948d9e6a556abeebe
Louis Shih <louis.shiru@gmail.com>
2025-04-15 20:44:14 +0800

    B

---
+++ leetcode/q.cpp
@@ -0,0 +1,29 @@
+5. Longest Palindromic Substring
+Medium
+Topics
+Companies
+Hint
+
+Given a string s, return the longest
+
+in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.
